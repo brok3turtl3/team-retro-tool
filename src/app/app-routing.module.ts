64e8100629edf39757e8959f6d9c6b022b25f58c
Sean Kelly <brok3turtl3@gmail.com>
2024-12-04 17:50:20 -0500

    Added routing logic for landing page and admin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,23 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'board', pathMatch: 'full' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./modules/landing-page/landing-page.module').then(
+        (m) => m.LandingPageModule
+      ),
+  },
   {
     path: 'board',
     loadChildren: () =>
       import('./modules/board/board.module').then((m) => m.BoardModule),
+  },
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('./modules/admin/admin.module').then((m) => m.AdminModule),
   },
 ];
 
